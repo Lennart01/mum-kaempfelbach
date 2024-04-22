0d1b8e8e7f455b6b97e5e606c97aafa303f1d097
Lennart Krauch <lennart.krauch@inovex.de>
2024-04-22 20:21:50 +0200

    initial commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'MUM Kämpfelbach';
+export const SITE_DESCRIPTION = 'Website von Mensch & Umwelt Kämpfelbach';
